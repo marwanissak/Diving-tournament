0b8a7fbdeea53b680f8131990e97242b21e6ac38
marwan <marwanissak@hotmail.com>
2019-07-18 11:52:36 +0200

    Initial commit

---
+++ .svn/pristine/88/888f382b7b8d502774ab45560c1da78d6ce5381f.svn-base
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Diving_organizer
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        /// Den är jättebra
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
---
+++ Diving_organizer/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Diving_organizer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Admin Admin_form = new Admin();
+            Admin_form.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Judge Judge_form = new Judge();
+            Judge_form.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Ranking Ranking_from = new Ranking();
+            Ranking_from.Show();
+        }
+    }
+
+}
